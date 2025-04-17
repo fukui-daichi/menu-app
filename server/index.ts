0e9aa8d793b6e8c4100777d266a837fed4af25e0
fukui-daichi <k.fukui0913@gmail.com>
2025-04-18 00:18:16 +0900

    デプロイ環境の設定

--- server/index.ts
+++ server/index.ts
@@ -14,9 +14,10 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type']
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
 }));
 app.use(express.json());
 
